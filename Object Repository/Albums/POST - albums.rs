d6a2fb1220df25978fdf1ce3cb6ff94f88262245
BRI <BRI@DELL-5SC8T74HKQ.Dlink>
2022-11-29 18:15:32 +0700

    post albums

--- Object Repository/Albums/POST - albums.rs
+++ Object Repository/Albums/POST - albums.rs
@@ -1,16 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
    <description></description>
-   <name>POST - posts</name>
+   <name>POST - albums</name>
    <tag></tag>
-   <elementGuidId>cea23e93-f883-4b9a-8f12-e8095051d75e</elementGuidId>
+   <elementGuidId>1445910c-ac04-47d1-be4a-93171fadc431</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <connectionTimeout>-1</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
    <httpBodyContent>{
-  &quot;text&quot;: &quot;{\n   \&quot;title\&quot;: \&quot;${GlobalVariable.title}\&quot;,\n   \&quot;body\&quot;: \&quot;${GlobalVariable.body}\&quot;,\n   \&quot;userId\&quot;: 1\n}&quot;,
+  &quot;text&quot;: &quot;{\n    \&quot;userId\&quot;: 1,\n    \&quot;id\&quot;: \&quot;${GlobalVariable.id}\&quot;,\n    \&quot;title\&quot;: \&quot;${GlobalVariable.title}\&quot;\n  }&quot;,
   &quot;contentType&quot;: &quot;application/json&quot;,
   &quot;charset&quot;: &quot;UTF-8&quot;
 }</httpBodyContent>
@@ -21,13 +21,13 @@
       <name>Content-Type</name>
       <type>Main</type>
       <value>application/json</value>
-      <webElementGuid>7de7563b-c3f6-4350-831d-3f7ce9d87c57</webElementGuid>
+      <webElementGuid>6167aba6-878f-47ca-aaf6-136176659114</webElementGuid>
    </httpHeaderProperties>
    <katalonVersion>8.5.0</katalonVersion>
    <maxResponseSize>-1</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>POST</restRequestMethod>
-   <restUrl>https://jsonplaceholder.typicode.com/posts</restUrl>
+   <restUrl>https://jsonplaceholder.typicode.com/albums</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
@@ -48,7 +48,6 @@ import internal.GlobalVariable as GlobalVariable
 
 RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 
-ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
-</verificationScript>
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
